662b9c2508e88a67cf5e77ab62d28d2b19f5e4e4
Manditar2 <76417604+Manditar2@users.noreply.github.com>
2023-05-24 15:57:19 -0400

    Update consumidores.py

--- kafka/consumidores/consumidores.py
+++ kafka/consumidores/consumidores.py
@@ -1,19 +1,84 @@
 import random
 import threading
+import time
 from kafka import KafkaConsumer
 from json import loads
-from time import sleep
 
-consumer = KafkaConsumer(
-    'topic_test',
-    bootstrap_servers='172.20.0.3:9093',
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='nuego_grupo',
-    value_deserializer=lambda x: loads(x.decode('utf-8'))
-)
+hilos = []
+bootstrapServer =  '172.19.0.3:9093'
+topicos = ['topic0','topic1','topic2','topic3','topic4']
+particiones_topico = [1,3,3,4,5]
 
-for event in consumer:
-    event_data = event.value
-    print(event_data)
-    sleep(2)
+def consumir(topico,bootstrapServer): 
+    consumer = KafkaConsumer(
+    topico,
+    bootstrap_servers=[bootstrapServer],
+    group_id='nuevo',
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    consumer.subscribe(topics=['topic1'])
+    print(consumer.partitions_for_topic('topic1'))
+
+    for event in consumer:
+        print("hola")
+        inicio = time.time()
+        event_data = event.value
+        final = time.time()
+        tiempo = final - inicio
+        print(event_data)
+
+        lock.acquire()
+
+        try:
+            with open('tiempos_consumidor.txt', 'a') as file:
+                file.write('\n' + f"{tiempo}")
+        finally:
+            lock.release()
+    consumer.close()
+    return
+
+def mConsumidores(topico): #Pregunta 5
+    consumer = KafkaConsumer(
+    topico,
+    bootstrap_servers=['172.19.0.3:9093'],
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+
+    for event in consumer:
+        inicio = time.time()
+        event_data = event.value
+        final = time.time()
+        tiempo = final - inicio
+        print('Estoy en el canal : ' + topico + ' Mensaje : ' + str(event_data))
+        lock.acquire()
+        try:
+            with open('tiempos_consumidor.txt', 'a') as file:
+                file.write('\n' + f"{tiempo}")
+        finally:
+            lock.release()
+    consumer.close()
+    return
+
+
+
+lock = threading.Lock()
+
+
+
+
+########################################################### Pregunta 5 #############################################################
+# n = 5
+
+
+# for i in range(n):
+#     consumidores_canal = random.randint(1,2) 
+#     for j in range(consumidores_canal):
+#         hilo = threading.Thread(target = mConsumidores, args=(topicos[i],))
+#         hilo.start()
+#         hilos.append(hilo)
+
+# for hilo in hilos:
+#     hilo.join()
+
+##################################################### Escalabilidad particiones #################################################
+
